30a4811ca2923d52e5dfe902f35dec3f99fa48fe
Ireneusz Kordal - QR75OV <ireneusz.kordal@ing.pl>
2022-09-14 18:00:44 +0200

    boot:v2.7.3 Mojarra:2.2.2 Tomcat/9.0.65
    (cherry picked from commit 67dae0f2ae175b2ca75305acd122bbcf8109c9dd)

--- src/main/java/com/example/bulbator/Application.java
+++ src/main/java/com/example/bulbator/Application.java
@@ -5,11 +5,8 @@ import javax.servlet.ServletContext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
-import org.springframework.boot.context.embedded.ServletContextInitializer;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/bulbator/ziutek/HelloBean.java
+++ src/main/java/com/example/bulbator/ziutek/HelloBean.java
@@ -3,11 +3,13 @@ package com.example.bulbator.ziutek;
 import org.springframework.stereotype.Component;
 
 import javax.faces.bean.ViewScoped;
+import java.util.Random;
 
 @Component
 @ViewScoped
 public class HelloBean {
     public String getMsg() {
-        return "Hello bubba";
+        Random random = new Random();
+        return "Hello bubba: " + random.nextInt(30);
     }
 }
